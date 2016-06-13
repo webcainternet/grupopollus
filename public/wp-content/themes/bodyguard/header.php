201159dfdaf4e82921b48fe47b9fd36a6f4ac3c9
fernandofmendes <fernando.mendes@webca.com.br>
2016-06-12 23:40:11 -0300

    GA

--- public/wp-content/themes/bodyguard/header.php
+++ public/wp-content/themes/bodyguard/header.php
@@ -17,6 +17,17 @@
 			padding-top: 20px !important;
 		}
 		</style>
+
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-67043306-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 	</head>
 	<?php if(wd_get_option('wd_box_wrapper','of')=='on') {$box='wd_wrapper';$body_bg='bg_body_color';}else{$box='';$body_bg='';} ?>
 	
